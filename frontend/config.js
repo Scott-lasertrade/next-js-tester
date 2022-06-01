373da41e2cac272fa64a4e0589fab789ca8a25bd
Scott-Lasertrade <scott@lasertrade.com.au>
2022-06-01 15:06:17 +1000

    Add default values

--- frontend/config.js
+++ frontend/config.js
@@ -1,16 +1,16 @@
 const config = {
   // Backend config
-  STAGE: process.env.NEXT_PUBLIC_STAGE,
+  STAGE: process.env.NEXT_PUBLIC_STAGE ?? "",
   apiGateway: {
-    REGION: process.env.NEXT_PUBLIC_REGION,
-    URL: process.env.NEXT_PUBLIC_API_URL,
+    REGION: process.env.NEXT_PUBLIC_REGION ?? "",
+    URL: process.env.NEXT_PUBLIC_API_URL ?? "",
   },
   cognito: {
-    REGION: process.env.NEXT_PUBLIC_REGION,
-    USER_POOL_ID: process.env.NEXT_PUBLIC_USER_POOL_ID,
-    APP_CLIENT_ID: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID,
-    IDENTITY_POOL_ID: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID,
-    OAUTH_DOMAIN: process.env.NEXT_PUBLIC_COGNITO_DOMAIN,
+    REGION: process.env.NEXT_PUBLIC_REGION ?? "",
+    USER_POOL_ID: process.env.NEXT_PUBLIC_USER_POOL_ID ?? "",
+    APP_CLIENT_ID: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID ?? "",
+    IDENTITY_POOL_ID: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID ?? "",
+    OAUTH_DOMAIN: process.env.NEXT_PUBLIC_COGNITO_DOMAIN ?? "",
   },
 };
 
